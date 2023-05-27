6e1a89ee747fb0990d0b765bd9c7e8d54368484a
ezekiel dombissi <66733800+theopen4@users.noreply.github.com>
2023-05-28 00:22:12 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,54 +1,51 @@
 #!/usr/bin/python3
 """
-N queens
+Try and solve the N Queens problem
 """
 
 import sys
 
-if len(sys.argv) != 2:
-    print('Usage: nqueens N')
-    exit(1)
-
-try:
-    n_q = int(sys.argv[1])
-except ValueError:
-    print('N must be a number')
-    exit(1)
-
-if n_q < 4:
-    print('N must be at least 4')
-    exit(1)
-
 
 def solve_nqueens(n):
-    '''self descriptive'''
-    if n == 0:
-        return [[]]
-    inner_solution = solve_nqueens(n - 1)
-    return [solution + [(n, i + 1)]
-            for i in range(n_q)
-            for solution in inner_solution
-            if safe_queen((n, i + 1), solution)]
+    if not isinstance(n, int):
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    queens = [-1] * n
+    solutions = []
+    solve(queens, 0, n, solutions)
+    for solution in solutions:
+        positions = [[i, q] for i, q in enumerate(solution)]
+        print(positions)
 
 
-def attack_queen(square, queen):
-    '''self descriptive'''
-    (row1, col1) = square
-    (row2, col2) = queen
-    return (row1 == row2) or (col1 == col2) or\
-        abs(row1 - row2) == abs(col1 - col2)
+def solve(queens, row, n, solutions):
+    if row == n:
+        solutions.append(list(queens))
+    else:
+        for col in range(n):
+            if is_valid(queens, row, col):
+                queens[row] = col
+                solve(queens, row + 1, n, solutions)
 
 
-def safe_queen(sqr, queens):
-    '''self descriptive'''
-    for queen in queens:
-        if attack_queen(sqr, queen):
+def is_valid(queens, row, col):
+    for r in range(row):
+        c = queens[r]
+        if c == col or abs(row - r) == abs(col - c):
             return False
     return True
 
 
-for answer in reversed(solve_nqueens(n_q)):
-    result = []
-    for p in [list(p) for p in answer]:
-        result.append([i - 1 for i in p])
-    print(result)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    solve_nqueens(n)
